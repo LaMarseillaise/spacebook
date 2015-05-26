64681ca8855085ffc2d1b547dd1c078e2ccdd4ca
Andur Carr <carr.andur@gmail.com>
2015-05-25 20:12:43 -0700

    Change user_spec terminology

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user){ FactoryGirl.create(:user) }
+  let(:user){ FactoryGirl.create :user }
 
-  describe 'associations' do
-    it { is_expected.to have_one(:profile) }
-    it { is_expected.to have_one(:cover_photo) }
+  context 'associations' do
+    it { is_expected.to have_one :profile }
+    it { is_expected.to have_one :cover_photo }
 
-    it { is_expected.to have_many(:posts) }
-    it { is_expected.to have_many(:comments) }
-    it { is_expected.to have_many(:photos) }
-    it { is_expected.to have_many(:likes) }
-    it { is_expected.to have_many(:initiated_friendings) }
-    it { is_expected.to have_many(:friended_users) }
-    it { is_expected.to have_many(:received_friendings) }
-    it { is_expected.to have_many(:users_friended_by) }
+    it { is_expected.to have_many :posts }
+    it { is_expected.to have_many :comments }
+    it { is_expected.to have_many :photos }
+    it { is_expected.to have_many :likes }
+    it { is_expected.to have_many :initiated_friendings }
+    it { is_expected.to have_many :friended_users }
+    it { is_expected.to have_many :received_friendings }
+    it { is_expected.to have_many :users_friended_by }
   end
 
-  describe 'validations' do
-    it { is_expected.to validate_presence_of(:first_name) }
-    it { is_expected.to validate_presence_of(:last_name) }
-    it { is_expected.to validate_presence_of(:email) }
+  context 'validations' do
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_presence_of :email }
 
     it 'should validate tag uniqueness of email' do
-      FactoryGirl.create(:user)
-      should validate_uniqueness_of(:email)
+      FactoryGirl.create :user
+      should validate_uniqueness_of :email
     end
 
     it { is_expected.to validate_length_of(:password).is_at_least(8) }
@@ -33,7 +33,7 @@ RSpec.describe User, type: :model do
           in_array(["Female", "Male", "Other"]) }
   end
 
-  describe 'instance methods' do
+  context 'instance methods' do
     describe '#name' do
       subject { user.name }
       it { is_expected.to eq(user.first_name + " " + user.last_name) }
@@ -62,17 +62,17 @@ RSpec.describe User, type: :model do
       describe '#friend_requests' do
         subject { user.friend_requests }
 
-        it { is_expected.not_to include(friend) }
-        it { is_expected.to include(friending_user) }
-        it { is_expected.not_to include(friended_user) }
+        it { is_expected.not_to include friend }
+        it { is_expected.to include friending_user }
+        it { is_expected.not_to include friended_user }
       end
 
       describe 'friends' do
         subject { user.friends }
 
-        it { is_expected.to include(friend) }
-        it { is_expected.not_to include(friending_user) }
-        it { is_expected.not_to include(friended_user) }
+        it { is_expected.to include friend }
+        it { is_expected.not_to include friending_user }
+        it { is_expected.not_to include friended_user }
       end
     end
   end
