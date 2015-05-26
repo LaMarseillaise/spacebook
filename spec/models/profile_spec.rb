79f1ef6877863051b8e34845e5fc2e94aed744b1
Andur Carr <carr.andur@gmail.com>
2015-05-25 17:19:46 -0700

    Build tests for profile model

---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Profile, type: :model do
+  describe 'associations' do
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :photo }
+    it { is_expected.to belong_to :cover_photo }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_length_of(:quotes).is_at_most(1000) }
+    it { is_expected.to validate_length_of(:about).is_at_most(1000) }
+
+    it { is_expected.to validate_length_of(:school).is_at_most(255) }
+    it { is_expected.to validate_length_of(:hometown).is_at_most(255) }
+    it { is_expected.to validate_length_of(:current_town).is_at_most(255) }
+    it { is_expected.to validate_length_of(:phone_number).is_at_most(255) }
+
+    it 'should validate the presence of the user' do
+      FactoryGirl.create(:profile)
+      should validate_presence_of(:user)
+    end
+  end
+end
