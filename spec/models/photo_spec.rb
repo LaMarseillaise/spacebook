9f158bf9880990d4958f1d0707da0e26ab9a502a
Andur Carr <carr.andur@gmail.com>
2015-05-25 23:12:16 -0700

    Begin photo model tests

---
+++ spec/models/photo_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Photo, type: :model do
+  context 'associations' do
+    it { is_expected.to belong_to(:author).class_name('User') }
+
+    it { is_expected.to have_many(:likes).dependent(:destroy) }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
+
+    it { is_expected.to have_attached_file(:photo) }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :author }
+
+    # it { is_expected.to validate_attachment_content_type(:photo).
+    #       allowing('').
+    #       rejecting('') }
+
+    it { is_expected.to validate_attachment_presence :photo }
+    it { is_expected.to validate_attachment_size(:photo).in(0..2.megabytes) }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'shoulda/matchers'
+require 'paperclip/matchers'
 require 'capybara/rails'
 require 'capybara/rspec'
 
@@ -15,4 +16,5 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  config.include Paperclip::Shoulda::Matchers
 end
