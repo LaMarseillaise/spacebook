e6d28bad7ffd8c0748732f5c55bcdbd354f63ce9
Andur Carr <carr.andur@gmail.com>
2015-05-30 16:15:55 -0700

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :user, aliases: [:author, :liker] do
-    first_name "John"
-    last_name "Doe"
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
     sequence(:email) { |i| "John#{i}@example.com" }
     password "password"
-    gender "Male"
+    gender ["Female", "Male", "Other"].sample
     birthday Date.today - 21.years
   end
 end
